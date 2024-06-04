91ed2b34a8646d25ed6e4aeeb36fa9625451a134
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-06-04 12:00:12 +0300

    Add files via upload

---
+++ Homework3_corr.py
@@ -0,0 +1,16 @@
+name = 'Вячеслав'
+print(name)
+age = 47
+print(age)
+age = age + 10
+print(age)
+
+is_student = True
+print(is_student)
+
+# extra examples by my side
+a = 5
+b = '10'
+print(a + int(b), type(a + int(b))) # сложение чисел
+print(str(a) + b, type(str(a) + b)) # сложение строк
+
